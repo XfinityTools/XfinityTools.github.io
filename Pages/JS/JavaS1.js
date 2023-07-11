f4b264144fb172c188b59e03ac5b34b509345c7e
XfinityTools <138805049+XfinityTools@users.noreply.github.com>
2023-07-11 15:38:06 -0500

    Add files via upload

--- Pages/JS/JavaS1.js
+++ Pages/JS/JavaS1.js
@@ -1,3 +1,66 @@
+function createZip() {
+  var input = document.getElementById('file-input');
+  var files = input.files;
+
+  if (files.length === 0) {
+    alert('Please select files to zip.');
+    return;
+  }
+
+  var zip = new JSZip();
+
+  function addFilesToZip(index) {
+    if (index >= files.length) {
+      zip.generateAsync({ type: 'blob' })
+        .then(function(content) {
+          saveAs(content, 'files.zip');
+        })
+        .catch(function(error) {
+          console.error('Error creating zip file:', error);
+        });
+      return;
+    }
+
+    var file = files[index];
+    var reader = new FileReader();
+
+    reader.onload = function() {
+      zip.file(file.name, reader.result);
+      addFilesToZip(index + 1);
+    };
+
+    reader.onerror = function() {
+      console.error('Error reading file:', file);
+      addFilesToZip(index + 1);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  addFilesToZip(0);
+}
+
+window.onload = function() {
+  var inputFile = document.getElementById('file-input');
+  var downloadButton = document.getElementById('downloadButton');
+  var fileList = document.getElementById('fileList');
+
+  inputFile.addEventListener('change', function(event) {
+    var files = event.target.files;
+    fileList.innerHTML = '';
+
+    for (var i = 0; i < files.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = files[i].name;
+      fileList.appendChild(listItem);
+    }
+  });
+
+  downloadButton.addEventListener('click', createZip);
+};
+
+
+
 function capitalizeAllLetters(event) {
     // Check if the Enter key was pressed (key code 13)
     if (event.keyCode === 13) {
@@ -90,7 +153,9 @@ function capitalizeAllLetters(event) {
         x[i].style.display="inline";				
       }
     }
-  }document.addEventListener('DOMContentLoaded', function() {
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
     var fileInput = document.getElementById('fileInput');
     var canvas = document.getElementById('canvas');
     var convertButton = document.getElementById('convertButton');
@@ -242,3 +307,4 @@ async function mergePDFs(inputPaths, outputPath) {
   downloadLink.download = outputPath;
   downloadLink.click();
 }
+
