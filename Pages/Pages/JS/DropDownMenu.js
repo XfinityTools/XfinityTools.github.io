652bc685fd2debec2e07cc77ac13eee24912eaad
XfinityTools <138805049+XfinityTools@users.noreply.github.com>
2024-12-03 15:19:56 -0500

    Delete Pages directory

--- Pages/Pages/JS/DropDownMenu.js
+++
@@ -1,9 +0,0 @@
-﻿// Dropdown functionality
-const dropdown = document.querySelector('.dropdown');
-const dropdownContent = document.querySelector('.dropdown-content');
-dropdown.addEventListener('mouseenter', () => {
-    dropdownContent.style.display = 'block';
-});
-dropdown.addEventListener('mouseleave', () => {
-    dropdownContent.style.display = 'none';
-});
